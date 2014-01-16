28d1da58be853cb097d51b163ff902705f7bd320
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-01-16 17:27:05 +0000

    Refactor KeychainItem class

--- openpassword/keychain_item.py
+++ openpassword/keychain_item.py
@@ -16,27 +16,30 @@ class KeychainItem:
         self.encrypted = None
         self.data = data
 
-    def encrypt(self, original_key):
-        keygen_iv = self._generate_iv()
-        derived_key = self._derive_key(original_key, keygen_iv)
-
+    def _encrypt(self, initialisation_vector, original_key):
         data = json.dumps(self.data)
         data = byte_pad(data.encode('utf8'))
-        data = encrypt(data, derived_key)
+        data = encrypt(data, self._derive_key(original_key, initialisation_vector))
+        return data
 
-        self.encrypted = b''.join(['Salted__'.encode('utf8'), keygen_iv, data])
+    def encrypt(self, original_key):
+        initialisation_vector = self._generate_iv()
+        encrypted_data = self._encrypt(initialisation_vector, original_key)
+
+        self.encrypted = b''.join(
+            ['Salted__'.encode('utf8'), initialisation_vector, encrypted_data])
 
     def decrypt(self, original_key):
-        keygen_iv = self.encrypted[8:16]
-        derived_key = self._derive_key(original_key, keygen_iv)
-
-        data = decrypt(self.encrypted[16:], derived_key)
+        data = decrypt(self.encrypted[16:], self._derive_key(original_key, self._extract_iv()))
         data = strip_byte_padding(data)
 
         self.data = json.loads(data.decode('utf8'))
 
     def _generate_iv(self):
         return urandom(8)
+
+    def _extract_iv(self):
+        return self.encrypted[8:16]
 
     def _derive_key(self, key, iv):
         return derive_openssl_key(key, iv)
